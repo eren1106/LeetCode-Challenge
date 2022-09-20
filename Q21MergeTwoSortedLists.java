9fd71f1d8fba0f084c49d3dd1b8757779255800a
eren <erenkuek1106@gmail.com>
2022-09-20 15:59:51 +0800

    21. Merge Two Sorted Lists

---
+++ Q21MergeTwoSortedLists.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null && list2 == null) return null;
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        
+        ListNode head = null, pointer = null;
+        if(list1.val < list2.val){
+            head = list1;
+            list1 = list1.next;
+        }
+        else{
+            head = list2;
+            list2 = list2.next;
+        }
+        pointer = head;
+        
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                pointer.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                pointer.next = list2;
+                list2 = list2.next;
+            }
+            pointer = pointer.next;
+        }
+        
+        if(list1 == null) pointer.next = list2;
+        if(list2 == null) pointer.next = list1;
+        
+        return head;
+    }
+}
