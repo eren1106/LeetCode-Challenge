2bc5b849f290498351a3f00eeea8ebdcfadcb7b3
eren <erenkuek1106@gmail.com>
2024-07-22 14:44:44 +0800

    update Q20

--- Q20ValidParentheses.java
+++ Q20ValidParentheses.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() % 2 != 0) return false;
         Stack<Character> stack = new Stack();
-        for(char c : s.toCharArray()){
-            if(c == '(' || c == '[' || c == '{'){
-                stack.push(c);
-            }
-            else{
-                if(stack.isEmpty()) return false;
-                char pop = stack.pop();
-                if(c == ')' && pop != '(') return false;
-                if(c == ']' && pop != '[') return false;
-                if(c == '}' && pop != '{') return false;
-            }
+
+        for(char c : s.toCharArray()) {
+            if(c == '(') stack.push(')');
+            else if(c == '{') stack.push('}');
+            else if(c == '[') stack.push(']');
+            else if(stack.isEmpty() || c != stack.pop()) return false;
         }
-        
+
         return stack.isEmpty();
     }
 }
