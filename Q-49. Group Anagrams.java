4e058cade344c014d31cefac71f75cc3cd507aff
eren <erenkuek1106@gmail.com>
2024-11-07 13:11:03 +0800

    49. Group Anagrams

---
+++ Q-49. Group Anagrams.java
@@ -0,0 +1,19 @@
+class Solution {
+  public List<List<String>> groupAnagrams(String[] strs) {
+      Map<String, List<String>> map = new HashMap<>();
+
+      for(String str : strs) {
+          char[] chars = str.toCharArray();
+          Arrays.sort(chars);
+          String sorted = new String(chars);
+
+          if(!map.containsKey(sorted)) {
+              map.put(sorted, new ArrayList<String>());
+          }
+
+          map.get(sorted).add(str);
+      }
+
+      return new ArrayList<>(map.values());
+  }
+}
