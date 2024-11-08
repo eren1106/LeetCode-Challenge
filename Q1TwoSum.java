df4fb76a08db90799c62ef7d4c44b5de80de2ba8
eren <erenkuek1106@gmail.com>
2024-11-08 23:56:20 +0800

    update q1

--- Q1TwoSum.java
+++ Q1TwoSum.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-
         for(int i = 0; i < nums.length; i++) {
-            int afterMinusTarget = target - nums[i];
-            if(map.containsKey(afterMinusTarget)) {
-                return new int[] {map.get(afterMinusTarget), i};
-            }
+            int afterMinus = target - nums[i];
+            if(map.containsKey(afterMinus)) return new int[]{map.get(afterMinus), i};
             map.put(nums[i], i);
         }
 
-        return new int[]{}; // no solution found
+        return new int[]{-1, -1}; // no solutions
     }
 }
